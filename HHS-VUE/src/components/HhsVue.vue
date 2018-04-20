ddeae67d9b0036e7d9d01ef2e7ff978783565529
yuanruixiangj <yuanruixiangjet@163.com>
2018-04-20 11:38:50 +0800

    0420

--- HHS-VUE/src/components/HhsVue.vue
+++ HHS-VUE/src/components/HhsVue.vue
@@ -4,15 +4,16 @@
     <div v-if="partData.partJson" class="pro-block" :class="showClass('part',index)" :key="index" v-for="(part,index) in partData.partJson" :id="partData.idJson[index+1]">
       <img class="ld" :pc-src="imgSrc('p',index)" :mb-src="imgSrc('m',index)" alt="">
       <template v-for="(linkId,index2) in partData.partJson[index]">
-        <a v-if="testHref(rules.toAnchor, linkId)" :key="index2" :href="linkId" class="plink" :class="showClass(linkPrefix.link,index2)"></a>
-        <a  v-if="testHref(rules.normalLink, linkId)" :key="index2" href="javascript:;" class="plink" :class="showClass(linkPrefix.link,index2)"></a>
+        <!-- <a v-if="testHref(rules.toAnchor, linkId)" :key="index2" :href="linkId" class="plink" :class="showClass(linkPrefix.link,index2)"></a> -->
+        <a v-if="testHref(rules.normalLink, linkId)" :key="index2" href="javascript:;" class="plink" :class="showClass(linkPrefix.link,index2)"></a>
         <template v-if="testHref(rules.prodLink, linkId)">  
-          <a :key="showKey(linkPrefix.link,index2)" :href="showProdHref(linkId)" class="plink" :class="showClass(linkPrefix.link,index2)" target="_blank"></a>
-          <a :key="showKey(linkPrefix.btn,index2)" :href="showProdHref(linkId)" class="plink js-button_shop_now" :class="showClass(linkPrefix.btn,index2)"></a>
+          <a track-id="00000000" id="9999999999999" :key="showKey(linkPrefix.link,index2)" :href="showProdHref(linkId)" class="plink" :class="showClass(linkPrefix.link,index2)" target="_blank"></a>
+          <a track-id="00000000" :key="showKey(linkPrefix.btn,index2)" :href="showProdHref(linkId)" class="plink js_quickshoppopin" :class="showClass(linkPrefix.btn,index2)"></a>
+          <a track-id="00000000" pop-id="11111" :key="index2" href="javascript:;" class="plink" :class="showClass(linkPrefix.click,index2)"></a>
         </template>
       </template>
     </div>
-  </div>
+  </div>  
 </template>
 
 <script>
@@ -23,12 +24,14 @@ export default {
     return {
       linkPrefix: {
         link: "link",
-        btn: "bt"
+        btn: "bt",
+        click: 'click'
       },
       rules: {
-        toAnchor: /^#/,
+        // toAnchor: /^#/,
         normalLink: /^\s*$/,
-        prodLink: /^BIO|[A-Za-z0-9]{5,}/
+        // urlLink: /[A-Za-z0-9]{1,5}/,
+        prodLink: /^LAN|[A-Za-z0-9]{5,}/
       },
       partData: partData
     };
